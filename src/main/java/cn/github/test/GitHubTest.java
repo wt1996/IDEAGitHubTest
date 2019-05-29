f61902fda9c4e5c157ea9962356077de6d904218
wangtao <17621903523@163.com>
2019-05-29 18:10:23 +0800

    第一次提交

---
+++ src/main/java/cn/github/test/GitHubTest.java
@@ -0,0 +1,18 @@
+package cn.github.test;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @ClassName GitHubTest
+ * @Author 桃子
+ * @Date 2019/5/29  18:03
+ * @Version 1.0
+ **/
+public class GitHubTest {
+
+    @Test
+    public void test1(){
+        System.out.println("GitHub进行第一次测试上传");
+    }
+}
